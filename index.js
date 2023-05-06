18bce920cad2bc711df1c301c94a3a7ef935a9f6
Sarath O S <sarathos584@gmail.com>
2023-05-06 12:44:57 +0530

    final commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express"); // imported express framework
 const dataservice = require("./services/data.service");
 const jwt = require("jsonwebtoken");
+const cors = require('cors') // cors library imported
 const app = express(); // created server application
+
+app.use(cors({
+  origin: "http://localhost:4200"
+}))
+
 app.use(express.json()); // to parse json to js
 const appMiddleware = (req, res, next) => {
   try {
     token = req.headers["x-access-token"];
     res = jwt.verify(token, "secretsuperkey1234");
+    req.currentAcno = res.currentAcno
     console.log(res);
     next();
   } catch {
@@ -16,7 +23,6 @@ const appMiddleware = (req, res, next) => {
       statusCode: 400,
     });
   }
-  
 };
 
 // app.use(appMiddleware);
@@ -34,14 +40,21 @@ app.post("/register", (req, res) => {
   // } else {
   //   res.status(result.statusCode).json(result);
   // }
-  res.send('success')
+  // res.send('success')
+  result.then((resObj) => {
+    res.status(resObj.statusCode).send(resObj);
+  });
 });
 
 //LOGIN API
 
 app.post("/login", (req, res) => {
   const result = dataservice.login(req.body.acno, req.body.pswd);
-  res.status(result.statusCode).json(result);
+  // res.status(result.statusCode).json(result);
+  result.then((resobj) => {
+    res.status(resobj.statusCode).send(resobj);
+  });
+  // res.send('success');
 });
 
 //DEPOSIT API
@@ -49,26 +62,44 @@ app.post("/deposit", appMiddleware, (req, res) => {
   const result = dataservice.deposit(
     req.body.acc,
     req.body.amnt,
-    req.body.pass
+    req.body.pass,
+    req
   );
-  res.status(result.statusCode).json(result);
+  result.then((resobj) => {
+    res.status(resobj.statusCode).send(resobj);
+  });
 });
 
 //  WITHDRAW API
-app.post("/withdraw",appMiddleware, (req, res) => {
+app.post("/withdraw", appMiddleware, (req, res) => {
   const result = dataservice.withdraw(
     req.body.acc,
     req.body.amnt,
     req.body.pass
   );
-  res.status(result.statusCode).json(result);
+  // res.status(result.statusCode).json(result);
+  result.then((resobj) => {
+    res.status(resobj.statusCode).send(resobj);
+  });
 });
 
 //transaction API
-app.post("/transact",appMiddleware, (req, res) => {
+app.post("/transact", appMiddleware, (req, res) => {
   const result = dataservice.getTransaction(req.body.acc);
-  res.status(result.statusCode).json(result);
+  // res.status(result.statusCode).json(result);
+  result.then((resobj) => {
+    res.status(resobj.statusCode).send(resobj);
+  });
 });
+
+// Account Delete Api 
+
+app.delete('/delacc/:acno',appMiddleware,(req,res)=>{
+  const result = dataservice.delAccount(req.params.acno)
+  result.then((resobj)=>{
+res.status(resobj.statusCode).send(resobj)
+  })
+})
 
 // configuring port number for server app
 app.listen(3000, () => {
--- services/data.service.js
+++ services/data.service.js
@@ -38,107 +38,108 @@ accounts = {
 const register = (acno, uname, phone, pswd) => {
   return db.Account.findOne({ account_no: acno }).then((acc) => {
     console.log(acc);
+    if (acc) {
+      return {
+        status: false,
+        message: "Account already exists!! Try Log in",
+        statusCode: 404,
+      };
+    } else {
+      let accnt = new db.Account({
+        account_no: acno,
+        name: uname,
+        phone: phone,
+        balance: 0,
+        password: pswd,
+        transaction: [],
+      });
+      accnt.save(); //by this method we are saving the data to db instead of insertOne method
+      return {
+        status: true,
+        message: "Registration completed!",
+        statusCode: 201,
+      };
+    }
   });
-
-  // if (acno in accounts) {
-  //   return {
-  //     status: false,
-  //     message: "Account already exists!! Try Log in",
-  //     statusCode: 404,
-  //   };
-  // } else {
-  //   accounts[acno] = {
-  //     account_no: acno,
-  //     name: uname,
-  //     phone: phone,
-  //     balance: 0,
-  //     password: pswd,
-  //     transaction: [],
-  //   };
-  //   console.log(accounts);
-  //   return {
-  //     status: true,
-  //     message: "Registration completed!",
-  //     statusCode: 201,
-  //   };
-  // }
 };
 
 const login = (acno, pswd) => {
-  if (acno in accounts) {
-    if (accounts[acno].password == pswd) {
-      currentUser = accounts[acno].name;
+  return db.Account.findOne({
+    account_no: acno,
+    password: pswd,
+  }).then((res) => {
+    // console.log(res);
+    if (res) {
+      currentUser = res.name;
       currentAcno = acno;
-      token = jwt.sign(
-        //first parameter is acno of current user in key value pair model
-        //second parameter is secret or private key
-        { currentAcno: acno },
-        "secretsuperkey1234"
-      );
+      token = jwt.sign({ currentAcno: acno }, "secretsuperkey1234");
       return {
         status: true,
         message: "login successfull",
         statusCode: 200,
         currentUser,
+        currentAcno,
         token,
       };
     } else {
       return {
         status: false,
-        message: "invalid password",
-        statusCode: 432,
-      };
-    }
-  } else {
-    return {
-      status: false,
-      message: "invalid account number",
-      statusCode: 400,
-    };
-  }
-};
-
-const deposit = (acc, amnt, pass) => {
-  if (acc in accounts) {
-    if (accounts[acc].password == pass) {
-      accounts[acc].balance += parseInt(amnt);
-      let history = { Type: "CREDIT", Amount: parseInt(amnt) };
-      accounts[acc].transaction.push(history);
-      return {
-        status: true,
-        message: `Deposit succesfull! Balance is ${accounts[acc].balance}`,
-        statusCode: 200,
-      };
-    } else {
-      return {
-        status: false,
-        message: "incorrect password",
+        message: "invalid Password or Account number",
         statusCode: 400,
       };
     }
-  } else {
-    return {
-      status: false,
-      message: "incorrect account number",
-      statusCode: 400,
-    };
-  }
+  });
+};
+
+const deposit = (acc, amnt, pass,req) => {
+  return db.Account.findOne({
+    account_no: acc,
+    password: pass,
+  }).then((res) => {
+    if (res) {
+      if(req.currentAcno!=res.account_no){
+        return{
+          status:false,
+          message:'Given Acno is not authenticated',
+          statusCode:422
+        }
+      }
+     else {res.balance += parseInt(amnt);
+      let history = { Type: "CREDIT", Amount: parseInt(amnt) };
+      res.transaction.push(history);
+      res.save();
+      return {
+        status: true,
+        message: "Amount deposited to your account. Balance is :" + res.balance,
+        statusCode: 200,
+      };}
+    } else {
+      return {
+        status: false,
+        message: "Incorrect Account number or Password",
+        statusCode: 400,
+      };
+    }
+  });
 };
 
 const withdraw = (acc, amnt, pass) => {
-  if (acc in accounts) {
-    if (accounts[acc].password == pass) {
-      if (accounts[acc].balance >= amnt) {
-        accounts[acc].balance -= parseInt(amnt);
+  return db.Account.findOne({
+    account_no: acc,
+    password: pass,
+  }).then((res) => {
+    if (res) {
+      if (res.balance >= amnt) {
+        res.balance -= parseInt(amnt);
         let history = { Type: "Debit", Amount: parseInt(amnt) };
-        accounts[acc].transaction.push(history);
+        res.transaction.push(history);
+        res.save();
         return {
           status: true,
-          message: `Withdrawal succesful! Current Balance : ${accounts[acc].balance}`,
+          message: `Withdrawal succesful! Current Balance : ${res.balance}`,
           statusCode: 200,
         };
       } else {
-        //insuficcient balance
         return {
           status: false,
           message: "insufficient balance",
@@ -148,32 +149,67 @@ const withdraw = (acc, amnt, pass) => {
     } else {
       return {
         status: false,
-        message: "incorrect password",
+        message: "Incorrect Account number or Password",
         statusCode: 400,
       };
     }
-  } else {
-    return {
-      status: false,
-      message: "incorrect account number",
-      statusCode: 400,
-    };
-  }
+  });
 };
 const getTransaction = (acc) => {
-  if (acc in accounts) {
-    return {
-      status: true,
-      message: "success",
-      data: accounts[acc].transaction,
-      statusCode: 200,
-    };
-  } else {
-    return {
-      status: false,
-      message: "invalid acc (login required)",
-      statusCode: 422,
-    };
-  }
-};
-module.exports = { register, login, deposit, withdraw, getTransaction };
+
+    return db.Account.findOne({
+      account_no:acc
+    }).then(res=>{
+      if(res){
+        return {
+              status: true,
+              message: "success",
+              data: res.transaction,
+              statusCode: 200,
+            };
+      }
+      else{
+        return {
+              status: false,
+              message: "invalid acc (login required)",
+              statusCode: 422,
+            };
+      }
+    })
+
+  };
+  // if (acc in accounts) {
+  //   return {
+  //     status: true,
+  //     message: "success",
+  //     data: accounts[acc].transaction,
+  //     statusCode: 200,
+  //   };
+  // } else {
+  //   return {
+  //     status: false,
+  //     message: "invalid acc (login required)",
+  //     statusCode: 422,
+  //   };
+  // }
+    const delAccount =(acno)=>{
+     return db.Account.deleteOne({account_no:acno}).then(res=>{
+        if(res){
+          return{
+            status:true,
+            message:'Deletion success',
+            statusCode:200
+          }
+        }
+        else{
+          return {
+            status:false,
+            message:"deletion failed",
+            statusCode:400
+          }
+        }
+      })
+    } 
+
+
+module.exports = { register, login, deposit, withdraw, getTransaction,delAccount };
